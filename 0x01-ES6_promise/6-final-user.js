cf47ac105b5cae45e9aa7a1cd9ba8d6ee67a0ddd
Madani00 <mamchart@gmail.com>
2024-07-24 19:35:04 +0000

    good

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,12 +3,12 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((results) => {
-        return results.map((result) => {
-            if (result.status === 'fulfilled') {
-                return { status: result.status, value: result.value };
-            } else {
-                return { status: result.status, value: result.reason };
-            }
-        });
+    return results.map((result) => {
+      if (result.status === 'fulfilled') {
+          return { status: result.status, value: result.value };
+      } else {
+          return { status: result.status, value: result.reason };
+      }
     });
+  });
 }
