4086bd6b829829b5bbadfab09afb98e541b635a4
Madani00 <mamchart@gmail.com>
2024-07-26 19:18:08 +0000

    good

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,7 +6,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const uploadPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([signUpPromise, uploadPromise])
-    .then((res) => res.map((result) => {
+    .then((results) => results.map((result) => {
       if (result.status === 'fulfilled') {
         return { status: result.status, value: result.value };
       }
