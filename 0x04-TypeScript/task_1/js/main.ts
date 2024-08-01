6c7a501d232c09305a261a10f35ac29c6f4a1522
Madani00 <mamchart@gmail.com>
2024-08-01 18:06:55 +0000

    good

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -12,10 +12,10 @@ interface Directors extends Teacher {
 }
 
 const teacher3: Teacher = {
-  firstName: 'John',
-  lastName: 'Doe',
+  firstName: 'john',
+  lastName: 'week',
   fullTimeEmployee: false,
-  location: 'London',
+  location: 'USA',
   contract: false,
 };
 
@@ -23,11 +23,45 @@ console.log(teacher3);
 
 
 const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
+  firstName: 'john',
+  lastName: 'week',
+  location: 'USA',
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
 
 console.log(director1);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(public firstName: string, public lastName: string) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass('john', 'week');
+console.log(student.workOnHomework());
+console.log(student.displayName());   
+
